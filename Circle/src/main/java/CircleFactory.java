ee8625696485adc3f2a32e06682b667c229f9d5f
Mark Provenzano <mark.pro@stratuscube.com>
2017-05-09 23:03:01 -0400

    changed the way CircleFactory works

--- Circle/src/main/java/CircleFactory.java
+++ Circle/src/main/java/CircleFactory.java
@@ -3,21 +3,25 @@ import java.lang.Math;
 public class CircleFactory {
     public static Circle createCircle(double radius) {
         Circle c = new Circle(radius);
-        c.setDiameter(calculateDiameter(radius));
-        c.setCircumference(calculateCircumference(radius));
-        c.setArea(calculateArea(radius));
+        c = calculateDiameter(c);
+        c = calculateCircumference(c);
+        c = calculateArea(c);
         return c;
     }
 
-    private static double calculateDiameter(double radius) {
-        return radius * 2;
+    private static Circle calculateDiameter(Circle circle) {
+        circle.setDiameter(circle.getRadius() * 2);
+        return circle;
     }
 
-    private static double calculateCircumference(double radius) {
-        return 2 * Math.PI * radius;
+    private static Circle calculateCircumference(Circle circle) {
+        circle.setCircumference(2 * Math.PI * circle.getRadius());
+        return circle;
     }
 
-    private static double calculateArea(double radius) {
-        return Math.PI * (radius * radius);
+    private static Circle calculateArea(Circle circle) {
+        double r = circle.getRadius();
+        circle.setArea(Math.PI * (r * r));
+        return circle;
     }
 }
