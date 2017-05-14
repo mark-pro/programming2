c5e54c68bd4c586632f4dc9f489f895d741de5f7
Mark Provenzano <mark.pro@stratuscube.com>
2017-05-13 23:31:11 -0400

    renamed profile creator to profile factory

---
+++ HealthProfile/src/main/java/HealthProfileFactory.java
@@ -0,0 +1,35 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+class HealthProfileFactory {
+    public static HealthProfile createProfileFromInput() {
+        try(Scanner in = new Scanner(System.in)) {
+            System.out.println("Enter in the patient's full name in (Doe, John)");
+            System.out.print("-> ");
+            String fullName = in.nextLine();
+            
+            System.out.println("Enter in the patient's gender");
+            System.out.print("-> ");
+            char gender = in.nextLine().charAt(0);
+
+            System.out.println("Enter in the patients date of birth (mm/dd/yyyy)");
+            System.out.print("-> "); 
+            String dob = in.nextLine();
+
+            System.out.println("Enter in the patient's height");
+            System.out.print("-> " );
+            int height = in.nextInt();
+
+            System.out.println("Enter in the patient's wight");
+            System.out.print("-> " );
+            int weight = in.nextInt();
+
+            String[] numbers = dob.split("/");
+            int month = Integer.parseInt(numbers[0]);
+            int day = Integer.parseInt(numbers[1]);
+            int year = Integer.parseInt(numbers[2]);
+
+            return new HealthProfile(
+                fullName, gender, LocalDate.of(year, month, day), height, weight);
+        }
+    }
+}
