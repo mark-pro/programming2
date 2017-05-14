d9415a700b6d245036c123ca73ad8819f15a7611
Mark Provenzano <mark.pro@stratuscube.com>
2017-05-14 14:28:51 -0400

    profile cannot be null. Will reprompt to make a profile

--- HealthProfile/src/main/java/Main.java
+++ HealthProfile/src/main/java/Main.java
@@ -9,7 +9,14 @@
  */
 public class Main {
     public static void main(String... args) {
-        HealthProfile profile = HealthProfileFactory.createProfileFromInput();
+        run();        
+    }
+
+    private static void run() {
+        HealthProfile profile;
+        do {
+            profile = HealthProfileFactory.createProfileFromInput();
+        } while(profile == null);
         System.out.println("//---------------------//");
         System.out.println(profile);
     }
