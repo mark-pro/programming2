7ec94546dc864ef1a271555d692405084de03db6
Mark Provenzano <mark.pro@stratuscube.com>
2017-05-15 17:49:04 -0400

    Factorial function added to main class.

--- Factorial/src/main/java/Main.java
+++ Factorial/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Copyright (C) 2017 Mark Pro.
  */
@@ -8,4 +10,15 @@
  * @version 1.0.0
  */
 public class Main {
+    public static void main(String... args) {
+        try(Scanner in = new Scanner(System.in)) {
+            System.out.print("Enter in a number greater than 0\n{~}> ");
+            int n = in.nextInt();
+            long result = calculateFactorial(n);
+            System.out.println(n + "! = " + result);
+        }
+    }
+    public static long calculateFactorial(int n) {
+        long i; for(i = 1; n > 0 ; i *= n--); return i;
+    }
 }
