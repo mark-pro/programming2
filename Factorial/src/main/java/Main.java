d264de6c4045858a2cc28e05b916dbc6a0ad3ba1
Mark Provenzano <mark.pro@stratuscube.com>
2017-05-16 14:09:30 -0400

    added factorial bounds of 0 < n < 20

--- Factorial/src/main/java/Main.java
+++ Factorial/src/main/java/Main.java
@@ -12,13 +12,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String... args) {
         try(Scanner in = new Scanner(System.in)) {
-            System.out.print("Enter in a number greater than 0\n{~}> ");
+            System.out.print("Enter in a number 0 < n < 21\n{~}> ");
             short n = in.nextShort();
-            long result = calculateFactorial(n);
-            System.out.println(n + "! = " + result);
+            if (inBounds(n)) {
+                long result = calculateFactorial(n);
+                System.out.println(n + "! = " + result);
+            }
+            else
+                System.out.printf("Could not calculate %d!\nnow exiting..." , n);
         }
     }
     public static long calculateFactorial(short n) {
-        long i = 1; for(; n > 0 ; i *= n--); return i;
+        long i = 1; for(; inBounds(n) ; i *= n--); return i;
+    }
+    private static boolean inBounds(short n) {
+        return 0 < n && n < 20;
     }
 }
