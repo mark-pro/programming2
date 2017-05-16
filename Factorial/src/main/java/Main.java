4c31f737de28a3af4a373c2beae2315fed07b2a7
Mark Provenzano <mark.pro@stratuscube.com>
2017-05-16 16:52:43 -0400

    updated the factorial program to have a sentinal loop for exit

--- Factorial/src/main/java/Main.java
+++ Factorial/src/main/java/Main.java
@@ -12,20 +12,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String... args) {
         try(Scanner in = new Scanner(System.in)) {
-            System.out.print("Enter in a number 0 < n < 21\n{~}> ");
-            short n = in.nextShort();
-            if (inBounds(n)) {
-                long result = calculateFactorial(n);
-                System.out.println(n + "! = " + result);
-            }
-            else
-                System.out.printf("Could not calculate %d!\nnow exiting..." , n);
+            short n = 0;
+            do {
+                System.out.print("Enter in a number -1 < n < 21\n{~}> ");
+                n = in.nextShort();
+                if (inBounds(n)) {
+                    long result = calculateFactorial(n);
+                    System.out.println(n + "! = " + result);
+                }
+                else
+                    System.out.printf("Could not calculate %d!\n" , n);
+            } while (!inBounds(n));
         }
     }
     public static long calculateFactorial(short n) {
-        long i = 1; for(; inBounds(n) ; i *= n--); return i;
+        long i = 1; for(n = n == 0 ? 1 : n ; n > 0 ; i *= n--); return i;
     }
     private static boolean inBounds(short n) {
-        return 0 < n && n < 20;
+        return -1 < n && n < 21;
     }
 }
