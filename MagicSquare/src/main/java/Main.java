2ba818e7b8c86f286a25e4fdbf9a09d24cdee8ea
Mark Provenzano <mark.pro@stratuscube.com>
2017-05-30 17:10:41 -0400

    Aded matric helper and the create matric method

--- MagicSquare/src/main/java/Main.java
+++ MagicSquare/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Copyright (C) 2017 Mark Pro.
  */
@@ -9,6 +11,20 @@
  */
 public class Main {
     public static void main(String[] args) {
-        
+        Run();
+    }
+
+    private static void Run() {
+        try(Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter in 9 numbers with spaces between them\n{~}>");
+            String numbers = input.nextLine();
+            int[][] nxn = MatrixHelper.createMatrix(numbers);
+            for(int i = 0 ; i < nxn.length; i++) {
+                for(int j = 0; j < nxn[0].length; j++)
+                    System.out.printf("%d ", nxn[i][j]);
+
+                System.out.println();
+            }
+        }
     }
 }
